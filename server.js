5526f768c5f0177d60d886705a44935316783784
Ravi Roshan <104033147+Ravi98351@users.noreply.github.com>
2023-01-20 21:58:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-const express = require('express')
 require('dotenv').config()
+const express = require('express')
+
+
  const connect=require("./db connect/db")
 const userRouter=require("./signup/sign.router")
- 
+ const cors =require('cors')
 const app=express()
-const cors =require('cors')
+
  const port=process.env.PORT;
 
 console.log(port)
 app.use(express.json())
 app.use(cors())
 
-app.use("/signup",userRouter)
+app.use("/users",userRouter)
 app.get('/',(req,res)=>res.send('hello'))
 
-app.listen(8080,async()=>{
+app.listen(port,async()=>{
    
         await connect()
 console.log(`listening at http://localhost:${port}`)
